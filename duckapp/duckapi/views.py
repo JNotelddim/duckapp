652a5a3f7b9c84f9a65ff72a862d06c4e9570ec5
JNotelddim <jaredmiddleton3.14@gmail.com>
2019-10-25 15:34:39 -0700

    Removed the viewsets for users + groups
    shouldn't be needing to add/remove users/groups dynamically at the
    moment. I don't see myself adding a registration page for the sake of
    this exercise. I'd probably rather just make a couple of accounts for
    the evaluators to use, and share those accounts with them.

--- duckapp/duckapi/views.py
+++ duckapp/duckapi/views.py
@@ -1,27 +1,10 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+
 from duckapp.duckapi.models import FeedingSession
-from duckapp.duckapi.serializers \
-    import UserSerializer, GroupSerializer, FeedingSessionSerializer
+from duckapp.duckapi.serializers import FeedingSessionSerializer
 
 
 # Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
 class FeedingSessionViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows feeding sessions to be viewed or edited
