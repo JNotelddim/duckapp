b434b43c858eaa39ba7d81fe7ccac16b66eed102
JNotelddim <jaredmiddleton3.14@gmail.com>
2019-10-25 15:12:13 -0700

    Create 'FeedingSession' model and apply migration

--- duckapp/duckapi/models.py
+++ duckapp/duckapi/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class FeedingSession(models.Model):
+    # feeding time
+    feeding_time = models.DateTimeField()
+
+    # food
+    food = models.CharField(max_length=100)
+    # food_type
+    food_type = models.CharField(max_length=100)
+    # food quantity
+    food_quantity = models.CharField(max_length=50)
+
+    # location
+    location = models.CharField(max_length=200)
+    # number of ducks
+    duck_count = models.IntegerField()
